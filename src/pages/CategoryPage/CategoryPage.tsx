74d793ae160637d8bd9814ae8f9194e6deaf91bc
MagiTom <mag.tomala1@gmail.com>
2024-01-14 14:58:33 +0100

    filtered

--- src/pages/CategoryPage/CategoryPage.tsx
+++ src/pages/CategoryPage/CategoryPage.tsx
@@ -4,10 +4,13 @@ import SongTitle from "../../components/SongTitle/SongTitle";
 import { useSongListContext } from "../../context/SongListContext";
 import { SongListLeft } from "../../models/SongListLeft.model";
 import "./style.scss";
+import { InputAdornment, TextField } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
 
 export const CategoryPage: React.FC<any> = () => {
   const { category } = useParams();
   const [songListItems, setSongListItems] = useState<SongListLeft[]>([]);
+  const [songListItemsCopy, setSongListItemsCopy] = useState<SongListLeft[]>([]);
   const { addSongRight, songListLeft } = useSongListContext();
   const navigate = useNavigate();
 
@@ -15,7 +18,9 @@ export const CategoryPage: React.FC<any> = () => {
     const findSongByCategory = songListLeft.filter(
       (song: SongListLeft) => song.category === category
     );
-    setSongListItems(category ? findSongByCategory : songListLeft);
+    const list = category ? findSongByCategory : songListLeft;
+    setSongListItems(list);
+    setSongListItemsCopy(list);
   }, [category, songListLeft]);
 
   const addSongItem = async (song: SongListLeft) => {
@@ -27,16 +32,41 @@ export const CategoryPage: React.FC<any> = () => {
     navigate(url);
   };
 
+  function setFilter(value: string): void {
+    const filteredData = [...songListItemsCopy].filter((obj) =>
+      obj.title.includes(value)
+    );
+    setSongListItems(filteredData);
+  }
+
   return (
-    <div className="list">
-      {songListItems?.map((song) => (
-        <SongTitle
-          goToPage={() => goPage(song.id)}
-          key={song.id}
-          addSongToList={addSongItem}
-          song={song}
-        />
-      ))}
+    <div className="category-page">
+      <TextField
+        color="success"
+        autoFocus
+        margin="dense"
+        label="szukaj"
+        fullWidth
+        variant="outlined"
+        InputProps={{
+          endAdornment: (
+            <InputAdornment position="start">
+              <SearchIcon />
+            </InputAdornment>
+          ),
+        }}
+        onChange={(e) => setFilter(e.target.value)}
+      />
+      <div className="list">
+        {songListItems?.map((song) => (
+          <SongTitle
+            goToPage={() => goPage(song.id)}
+            key={song.id}
+            addSongToList={addSongItem}
+            song={song}
+          />
+        ))}
+      </div>
     </div>
   );
 };
